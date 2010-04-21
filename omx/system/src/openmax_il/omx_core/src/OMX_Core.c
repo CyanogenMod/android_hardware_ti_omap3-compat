171f69f0862c38ffef8b7495326736757b41f333
Daniel Charles <dcharles@ti.com>
2010-04-21 17:33:41 -0500

    TI OMX Core: Handle free of multiple instances.
    When multiple instances of the same component are referenced, the order those
    instances will be unloaded will not follow a particular order.
    
    If an instance create before others is closed first, then the rest will create
    memory leaks.  They won't be freed although the component exits correctly.  This
    patch addresses this scenario by shifting the remaining instances in the array
    for further closure.
    
    Also some debug messages were enhaced to have a better understanding of which
    components are loaded/unloades based on their handles.
    
    Patch set 2:  Removes patch dependencies.
    
    Change-Id: Id112ce68c3e03895a00a18a81e209b8a019f89d2

--- omx/system/src/openmax_il/omx_core/src/OMX_Core.c
+++ omx/system/src/openmax_il/omx_core/src/OMX_Core.c
@@ -210,8 +210,6 @@ OMX_ERRORTYPE TIOMX_GetHandle( OMX_HANDLETYPE* pHandle, OMX_STRING cComponentNam
     for (refIndex=0; refIndex < MAX_TABLE_SIZE; refIndex++) {
         //get the index for the component in the table
         if (strcmp(componentTable[refIndex].name, cComponentName) == 0) {
-            LOGD("Found component %s with refCount %d\n",
-                  cComponentName, componentTable[refIndex].refCount);
 
             /* check if the component is already loaded */
             if (componentTable[refIndex].refCount >= MAX_CONCURRENT_INSTANCES) {
@@ -266,6 +264,8 @@ OMX_ERRORTYPE TIOMX_GetHandle( OMX_HANDLETYPE* pHandle, OMX_STRING cComponentNam
                     LOGE("%d:: malloc of pHandle* failed\n", __LINE__);
                     goto CLEAN_UP;
                 }
+                LOGD("Found component %s with refCount %d  pHandle (%p)\n",
+                        cComponentName, componentTable[refIndex].refCount, *pHandle);
 
                 pComponents[i] = *pHandle;
                 componentType = (OMX_COMPONENTTYPE*) *pHandle;
@@ -285,7 +285,7 @@ OMX_ERRORTYPE TIOMX_GetHandle( OMX_HANDLETYPE* pHandle, OMX_STRING cComponentNam
                     goto UNLOCK_MUTEX;  // Component is found, and thus we are done
                 }
                 else if (err == OMX_ErrorInsufficientResources) {
-                        LOGE("%d :: Core: Insufficient Resources for Component %d\n",__LINE__, err);
+                        LOGE("%d :: Core: Insufficient Resources for Component %x pHandle (%p)\n",__LINE__, err, *pHandle);
                         goto CLEAN_UP;
                 }
             }
@@ -361,17 +361,41 @@ OMX_ERRORTYPE TIOMX_FreeHandle (OMX_HANDLETYPE hComponent)
         goto EXIT;
     }
 
-    int refIndex = 0, handleIndex = 0;
+    int refIndex = 0, handleIndex = 0, shiftIndex=0;
     for (refIndex=0; refIndex < MAX_TABLE_SIZE; refIndex++) {
         for (handleIndex=0; handleIndex < componentTable[refIndex].refCount; handleIndex++){
             /* get the position for the component in the table */
             if (componentTable[refIndex].pHandle[handleIndex] == hComponent){
                 LOGD("Found matching pHandle(%p) at index %d with refCount %d",
                       hComponent, refIndex, componentTable[refIndex].refCount);
-                if (componentTable[refIndex].refCount) {
+                if (componentTable[refIndex].refCount > 1) {
+                    /*There is more than one instance of the same component. The
+                     * instance to free can be ahead of the last one created, so the rest of the
+                     * instances will be shifted in the array*/
+                    if (handleIndex < componentTable[refIndex].refCount-1)
+                    {
+                        /*This instance is not the last one created */
+                        for (shiftIndex=handleIndex; shiftIndex < componentTable[refIndex].refCount;shiftIndex++)
+                        {
+                            if (componentTable[refIndex].pHandle[shiftIndex])
+                            {
+                                componentTable[refIndex].pHandle[shiftIndex]= componentTable[refIndex].pHandle[shiftIndex+1];
+                                componentTable[refIndex].pHandle[shiftIndex+1] = NULL;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        /*This instance is the last one created */
+                        componentTable[refIndex].pHandle[handleIndex] = NULL;
+                    }
                     componentTable[refIndex].refCount -= 1;
                 }
-                componentTable[refIndex].pHandle[handleIndex] = NULL;
+                else
+                {
+                    componentTable[refIndex].refCount -= 1;
+                    componentTable[refIndex].pHandle[handleIndex] = NULL;
+                }
                 dlclose(pModules[i]);
                 pModules[i] = NULL;
                 free(pComponents[i]);
